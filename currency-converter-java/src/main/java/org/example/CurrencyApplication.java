961c053c057a6c1ab5f55147dc82b6ce735d2b38
Edgars <marchenkoedgarss@gmail.com>
2026-03-29 12:51:36 +0300

    -

--- currency-converter-java/src/main/java/org/example/CurrencyApplication.java
+++ currency-converter-java/src/main/java/org/example/CurrencyApplication.java
@@ -1,4 +1,13 @@
 package org.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class CurrencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(org.example.CurrencyApplication.class, args);
+    }
+
 }
